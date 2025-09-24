dd412c40b188e021696b99879224ec51f7e7c542
Technofancy Learner <Vkhatri137@gmail.com>
2025-09-24 21:50:50 +0545

    vite config changes build:dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Default output folder, ensure it's set to dist
+  },
   plugins: [react()],
   server: {
     proxy: {
