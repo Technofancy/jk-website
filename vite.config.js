01bef66325008457f04cb3f962afcdb85d6e75ec
Technofancy Learner <Vkhatri137@gmail.com>
2025-09-24 22:18:18 +0545

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -18,10 +18,10 @@ export default defineConfig({
   },
   define: {
     // Define a global variable based on environment
-    "process.env.VITE_API_URL": process.env.NODE_ENV === 'production' 
-      ? 'https://jarokilo.org.np' 
-      : 'http://localhost:3000', // You can adjust your dev URL accordingly
+    "process.env.VITE_API_URL": JSON.stringify(
+      process.env.NODE_ENV === 'production' 
+        ? 'https://jarokilo.org.np' 
+        : 'http://localhost:3000' // Adjust your dev URL accordingly
+    ),
   },
 });
-
-
