515f4efc26654f3cf0391efc7cd2a68c45428537
Technofancy Learner <Vkhatri137@gmail.com>
2025-09-24 14:10:46 +0545

    2nd edition just rough codes for lang-translation en and np

---
+++ src/i18n.js
@@ -0,0 +1,17 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "np",
+    resources: {
+      np: { translation: require("./locales/np/translation.json") },
+      en: { translation: require("./locales/en/translation.json") }
+    },
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
